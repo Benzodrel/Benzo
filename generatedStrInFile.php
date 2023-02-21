a9ea2bf547d9b5ff4c037afc8c7c9f2cf4e98ac3
Benzodrel <wizardofearth@yandex.ru>
2023-02-21 23:34:20 +0300

    format fix, nesting deletion

--- generatedStrInFile.php
+++ generatedStrInFile.php
@@ -4,7 +4,7 @@ function inFile(int $count, int $randmax)
     while ($count > 0) {
         $randomCharNum = mt_rand(1, $randmax);
         $count--;
-       file_put_contents(__DIR__ . '/Test_cases.txt', randomStr($randomCharNum), FILE_APPEND);
+        file_put_contents(__DIR__ . '/Test_cases.txt', randomStr($randomCharNum), FILE_APPEND);
         if ($count != 0) {
             file_put_contents(__DIR__ . '/Test_cases.txt', "\n", FILE_APPEND);
         }
