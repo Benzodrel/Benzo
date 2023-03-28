039c52ff5d1418e97fe6fc4264dabbbca4a6a9aa
Benzodrel <wizardofearth@yandex.ru>
2023-03-29 01:05:58 +0300

    register data now goes to database

--- registerHandler.php
+++ registerHandler.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-
+require_once 'config.php';
 require_once 'assistFunctions.php';
-if (count($_POST['registerData']) === 6){
+if (count($_POST['registerData']) === 6) {
     $_SESSION['userSurname'] = $_POST['registerData']['surname'];
     $_SESSION['userName'] = $_POST['registerData']['name'];
     $_SESSION['userEmail'] = $_POST['registerData']['email'];
@@ -19,13 +19,30 @@ $data = $_POST['registerData'];
 $image = $_FILES;
 $arrAll = getValidatedData($data, $image);
 if (empty($arrAll["error"])) {
-    $arrAll['data']['Password'] = md5($arrAll['data']['Password']);
-    $json_data = json_encode($arrAll['data'], JSON_UNESCAPED_UNICODE);
-    if (file_put_contents("users/{$_POST['registerData']['Login']}.json", $json_data) === false) {
-        $_SESSION['error']['saveData'] = 'Ошибка сохранения аватара';
-        header('Location: login.php');
+    $arrAll['data']['Password'] = password_hash($arrAll['data']['Password'], PASSWORD_BCRYPT);
+    $connect = mysqli_connect($host, $username, $password, $databaseName);
+    if ($connect === false) {
+        $_SESSION['error']['saveData'] = "Ошибка подключения в базе данных";
+        header('Location: register.php');
         die();
     }
+    $stmt = mysqli_prepare($connect, "INSERT INTO `users` (login, name, surname, email, password) VALUES (?,?,?,?,?)");
+    if ($stmt === false) {
+        $_SESSION['error']['saveData'] = "Ошибка подготовки SQL выражения";
+        header('Location: register.php');
+        die();
+    }
+    if (mysqli_stmt_bind_param($stmt, 'sssss', $arrAll['data']['Login'], $arrAll['data']['Name'], $arrAll['data']['Surname'], $arrAll['data']['Email'], $arrAll['data']['Password']) === false) {
+        $_SESSION['error']['saveData'] = "Ошибка привязки параметров подготовленного SQL выражения";
+        header('Location: register.php');
+        die();
+    }
+    if (mysqli_stmt_execute($stmt) === false) {
+        $_SESSION['error']['saveData'] = "Ошибка выполнения подготовленного SQL выражения";
+        header('Location: register.php');
+        die();
+    }
+    mysqli_close($connect);
     if (!empty($_FILES['avatar']['tmp_name'])) {
         if (move_uploaded_file($_FILES['avatar']['tmp_name'], $path) === false) {
             $_SESSION['error']['save'] = 'Ошибка сохранения аватара';
@@ -33,6 +50,7 @@ if (empty($arrAll["error"])) {
             die();
         }
     }
+
 } else {
     foreach ($arrAll["error"] as $key => $value) {
         $_SESSION['error'][$key] = $value;
